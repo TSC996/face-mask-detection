58b047d5549fc1abbfd6c9e7a35a12668b9e8d38
TEJAS <58255480+TSC996@users.noreply.github.com>
2021-08-19 18:01:47 +0530

    Update detect_mask_picam.py

--- detect_mask_picam.py
+++ detect_mask_picam.py
@@ -1,3 +1,4 @@
+#IN THIS CODE I EXCLUDED DOOR LOCK PART
 # import the necessary packages
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
